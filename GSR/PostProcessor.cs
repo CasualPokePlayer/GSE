648426fb893b42ca3c21b0883ed3c56002bcb7f3
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2024-04-07 12:45:52 -0700

    better fix

--- GSR/PostProcessor.cs
+++ GSR/PostProcessor.cs
@@ -30,11 +30,10 @@ internal sealed class PostProcessor(Config config, EmuManager emuManager, SDLRen
 	private readonly SDLTexture _blScaledTexture = new(sdlRenderer, SDL_PIXELFORMAT_ARGB8888,
 		SDL_TextureAccess.SDL_TEXTUREACCESS_TARGET, SDL_ScaleMode.SDL_ScaleModeLinear, SDL_BlendMode.SDL_BLENDMODE_NONE);
 
-	private (bool KeepAspectRatio, ScalingFilter VideoFilter, bool EmuTextureReset) _lastFrameConfig;
+	private (bool KeepAspectRatio, ScalingFilter VideoFilter, int EmuWidth, int EmuHeight) _lastFrameConfig;
 
 	public void ResetEmuTexture(int width, int height)
 	{
-		_lastFrameConfig.EmuTextureReset = true;
 		_emuTexture.ResetTexture(width, height);
 	}
 
@@ -124,7 +123,7 @@ internal sealed class PostProcessor(Config config, EmuManager emuManager, SDLRen
 	{
 		// check if the current config changed since last frame, we'll do additional clears in such a case
 		// don't need to check the window scaling here, finalWidth/finalHeight changing will cover that
-		var curConfig = (config.KeepAspectRatio, config.OutputFilter, false);
+		var curConfig = (config.KeepAspectRatio, config.OutputFilter, _emuTexture.Width, _emuTexture.Height);
 		var configChanged = curConfig != _lastFrameConfig;
 		_lastFrameConfig = curConfig;
 
