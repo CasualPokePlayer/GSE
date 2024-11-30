5a59d7f52c68516a91c6bca7247c3723880fa84e
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2024-11-30 00:01:39 -0800

    try this

--- android/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -780,14 +780,14 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
                                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.INVISIBLE;
                                 window.getDecorView().setSystemUiVisibility(flags);
-                                //window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-                                //window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                                window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+                                window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
                                 SDLActivity.mFullscreenModeActive = true;
                             } else {
                                 int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_VISIBLE;
                                 window.getDecorView().setSystemUiVisibility(flags);
-                                //window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-                                //window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                                window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                                window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
                                 SDLActivity.mFullscreenModeActive = false;
                             }
                         }
