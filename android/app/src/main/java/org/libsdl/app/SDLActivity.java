f52b7d9e587954be18881384667802cf8e2a6a70
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2024-11-30 00:19:12 -0800

    try this

--- android/app/src/main/java/org/libsdl/app/SDLActivity.java
+++ android/app/src/main/java/org/libsdl/app/SDLActivity.java
@@ -775,7 +775,7 @@ public class SDLActivity extends Activity implements View.OnSystemUiVisibilityCh
                             if ((msg.obj instanceof Integer) && ((Integer) msg.obj != 0)) {
                                 int flags = View.SYSTEM_UI_FLAG_FULLSCREEN |
                                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                        View.SYSTEM_UI_FLAG_IMMERSIVE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
                                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
                                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.INVISIBLE;
