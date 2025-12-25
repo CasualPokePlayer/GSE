7c5a63a55b4b1b1a2846ae172643550dc7246515
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2025-12-24 22:53:33 -0800

    update build script

--- .ci/linux.sh
+++ .ci/linux.sh
@@ -32,7 +32,7 @@ if [ $TARGET_RID = "linux-x64" ]; then
 		libxext-dev libxrandr-dev libxcursor-dev libxfixes-dev libxi-dev libxss-dev \
 		libxtst-dev libwayland-dev libxkbcommon-dev libdrm-dev libgbm-dev libgl1-mesa-dev \
 		libgles2-mesa-dev libegl1-mesa-dev libdbus-1-dev libibus-1.0-dev \
-		fcitx-libs-dev libudev-dev libusb-1.0-0-dev liburing-dev pkg-config
+		fcitx-libs-dev libudev-dev libusb-1.0-0-dev liburing-dev libthai-dev pkg-config
 	# More SDL3 dependencies only under backports
 	apt-get install -y libdecor-0-dev/bullseye-backports libpipewire-0.3-dev/bullseye-backports
 	# Install .NET AOT dependencies
@@ -55,7 +55,7 @@ elif [ $TARGET_RID = "linux-arm64" ]; then
 		libxrandr-dev:arm64 libxcursor-dev:arm64 libxfixes-dev:arm64 libxi-dev:arm64 libxss-dev:arm64 libxtst-dev:arm64 \
 		libwayland-dev:arm64 libxkbcommon-dev:arm64 libdrm-dev:arm64 libgbm-dev:arm64 libgl1-mesa-dev:arm64 \
 		libgles2-mesa-dev:arm64 libegl1-mesa-dev:arm64 libdbus-1-dev:arm64 libibus-1.0-dev:arm64 \
-		fcitx-libs-dev:arm64 libudev-dev:arm64 libusb-1.0-0-dev:arm64 liburing-dev:arm64
+		fcitx-libs-dev:arm64 libudev-dev:arm64 libusb-1.0-0-dev:arm64 liburing-dev:arm64 libthai-dev:arm64
 	# More SDL3 dependencies only under backports
 	apt-get install -y libdecor-0-dev:arm64/bullseye-backports libpipewire-0.3-dev:arm64/bullseye-backports
 	# Install .NET AOT dependencies
@@ -84,7 +84,7 @@ elif [ $TARGET_RID = "linux-arm" ]; then
 		libxrandr-dev:armhf libxcursor-dev:armhf libxfixes-dev:armhf libxi-dev:armhf libxss-dev:armhf libxtst-dev:armhf \
 		libwayland-dev:armhf libxkbcommon-dev:armhf libdrm-dev:armhf libgbm-dev:armhf libgl1-mesa-dev:armhf \
 		libgles2-mesa-dev:armhf libegl1-mesa-dev:armhf libdbus-1-dev:armhf libibus-1.0-dev:armhf fcitx-libs-dev:armhf \
-		libudev-dev:armhf libusb-1.0-0-dev:armhf liburing-dev:armhf libdecor-0-dev:armhf libpipewire-0.3-dev:armhf
+		libudev-dev:armhf libusb-1.0-0-dev:armhf liburing-dev:armhf libthai-dev:armhf libdecor-0-dev:armhf libpipewire-0.3-dev:armhf
 	# Install .NET AOT dependencies
 	apt-get install -y zlib1g-dev:armhf
 else
