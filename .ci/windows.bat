a7b47e4236c09971bc23849a52e3dc0effc35031
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2026-05-08 18:11:27 -0700

    Remove ilammy/msvc-dev-cmd in favor of inline impl (#30)
    ilammy/msvc-dev-cmd is apparently dead and is subject to break soon, it can be replaced with a simple inline impl

--- .ci/windows.bat
+++ .ci/windows.bat
@@ -1,10 +1,18 @@
-@:: Note: this script is expecting a VS Developer Command Prompt environment!
+:: Find the VS install path (so the correct VS build environment can be set)
+FOR /F "delims=" %%I IN (
+	'CALL "%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath'
+) DO (
+	SET VS_INSTALL_PATH=%%I
+)
 
 if "%TARGET_RID%" == "win-x64" (
+	CALL "%VS_INSTALL_PATH%\VC\Auxiliary\Build\vcvars64.bat"
 	SET EXTRA_CMAKE_ARGS=
 ) else if "%TARGET_RID%" == "win-arm64" (
+	CALL "%VS_INSTALL_PATH%\VC\Auxiliary\Build\vcvarsamd64_arm64.bat"
 	SET EXTRA_CMAKE_ARGS=-DCMAKE_SYSTEM_NAME=Windows -DCMAKE_SYSTEM_PROCESSOR=ARM64 -DCMAKE_C_FLAGS=--target=arm64-pc-windows-msvc -DCMAKE_CXX_FLAGS=--target=arm64-pc-windows-msvc
 ) else if "%TARGET_RID%" == "win-x86" (
+	CALL "%VS_INSTALL_PATH%\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
 	SET EXTRA_CMAKE_ARGS=-DCMAKE_SYSTEM_NAME=Windows -DCMAKE_SYSTEM_PROCESSOR=X86 -DCMAKE_C_FLAGS=--target=i686-pc-windows-msvc -DCMAKE_CXX_FLAGS=--target=i686-pc-windows-msvc
 ) else (
 	echo "Invalid TARGET_RID (got %TARGET_RID%)"
